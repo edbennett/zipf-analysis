76f1e183000071ac5a483bead92a1f82d870086e
Julian Lenz <j.j.lenz@swansea.ac.uk>
2023-05-23 12:33:46 +0100

    Add CL arguments to plotcounts

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -1,10 +1,23 @@
 """Plot word counts."""
 
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
+parser = argparse.ArgumentParser(description="Plot word counts")
+parser.add_argument('infile', type=argparse.FileType('r'),
+                    nargs='?', default='-',
+                       help='Word count csv file name')
+parser.add_argument('--xlim', type=float, nargs=2,
+                    metavar=('XMIN', 'XMAX'),
+                    default=None, help='X-axis limits')
+parser.add_argument('--outfile', type=str,
+                    default='plotcounts.png',
+                    help='Output image file name')
 
-df = pd.read_csv('results/frankenstein.csv', header=None,
+args = parser.parse_args()
+
+df = pd.read_csv(args.infile, header=None,
                  names=('word', 'word_frequency'))
 df['rank'] = df['word_frequency'].rank(ascending=False,
                                        method='max')
@@ -13,5 +26,6 @@ ax = df.plot.scatter(x='word_frequency',
                      y='inverse_rank',
                      figsize=[12, 6],
                      grid=True,
-                     xlim=None)
-plt.show()
+                     xlim=args.xlim)
+plt.savefig(args.outfile)
+
